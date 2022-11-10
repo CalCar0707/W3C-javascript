b60bbe2c685bda4459f8f2a222315c368c07908d
Call Carl <callienicole0419@gmail.com>
2022-11-10 10:16:10 -0600

    added global variables and prompt for pw length

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,37 @@
-// Assignment code here
+// Assignment Code
+// global variables here
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numberChoice = '0123456789';
+var specialCharacters = '~!@#$%^&*()_+';
+var finalPassword = [];
+var tempPassword;
+
+document.getElementById(password);
+document.getElementById(generate);
 
 
-// Get references to the #generate element
+/*if(includeLowercase === true){
+
+} else if(includeLowercase !==){
+
+}*/
+
+//prompt to select pw length between 8 - 128
+function generatePassword() {
+  var userInput = window.prompt('What would you like the length of your password to be?');
+  var passwordLength = userInput;
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.prompt('Please choose a length between 8 and 128 characters.')
+    } else {
+      console.log(passwordLength);
+    }
+}
+
+//prompt on what character types to include
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
