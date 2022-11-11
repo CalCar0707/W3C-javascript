b81478289b1197d46e5efe94014f43aebdc50ca0
Call Carl <callienicole0419@gmail.com>
2022-11-11 14:39:06 -0600

    updated readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 // global variables here
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var numberChoice = '0123456789';
-var specialCharacters = '~!@#$%^&*()_+';
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numberChoice = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialCharacters = ['~','!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+'];
 var finalPassword = [];
 var tempPassword;
 
@@ -34,6 +34,7 @@ var includeNumbers = window.confirm('Would you like to include any numbers?)');
 var includeSpecials = window.confirm('Would you like to include any special characters?');
     console.log(includeSpecials);
   }
+  
 
 var generateBtn = document.querySelector("#generate");
 
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Password Generator Starter Code
+
+## Description
+
+This application is a random password generator. It takes user input about what kind of password to create and provides a random password including said criteria. 
+
+## Usage
+
+
+![alt text](assets/images/PW-gen-screenshot.png)
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
